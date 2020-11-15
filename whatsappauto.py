925a1cbdb384c46bf1b2f4a83665ab1bcb20adb3
CodeEZ <muralidharan1307@gmail.com>
2020-11-15 16:19:07 +0530

    Update whatsappauto.py

--- whatsappauto.py
+++ whatsappauto.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
  
 account_sid = 'AC4887943c51b0899df889542d2bf46341' 
-auth_token = '4b4d7e3d571af74c64075b2693048e9d' 
+auth_token = 'xxxxx' 
 client = Client(account_sid, auth_token) 
 text_val = input("Enter your text to send: ")
 
